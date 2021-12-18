f5268298ee1f6a7b814d80852779fcdfa50dae6b
Means88 <means88.x@gmail.com>
2021-12-18 17:07:58 +0800

    fix: fuck

--- pages/api/bilibili.ts
+++ pages/api/bilibili.ts
@@ -1,7 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { fetchBiliBili } from '../data/bilibili';
-import { bilibiliCard } from '../render/bilibili';
+import { fetchBiliBili } from '../../data/bilibili';
+import { bilibiliCard } from '../../render/bilibili';
 
 export default async function handler(
   req: NextApiRequest,
