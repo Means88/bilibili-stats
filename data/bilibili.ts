a65cd66bfad8b432116e0f15ffefa9677c8388e3
Means88 <means88.x@gmail.com>
2022-09-22 21:29:01 +0800

    fix: bilibili anti crawler

--- data/bilibili.ts
+++ data/bilibili.ts
@@ -1,12 +1,33 @@
-import axios from 'axios';
+import axios, { AxiosRequestHeaders } from 'axios';
+
+const headers: AxiosRequestHeaders = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.123 Safari/537.36',
+  'sec-ch-ua': '"Chromium";v="104", " Not A;Brand";v="99", "Sidekick";v="104"',
+  'sec-ch-ua-mobile': '?0',
+  'sec-ch-ua-platform': "macOS",
+  'sec-fetch-dest': 'document',
+  'sec-fetch-mode': 'navigate',
+  'sec-fetch-site': 'none',
+  'sec-fetch-user': '?1',
+  'upgrade-insecure-requests': '1',
+}
+
+if (process.env.COOKIE) {
+  headers.cookie = process.env.COOKIE
+}
+
+const request = axios.create({
+  headers,
+})
 
 export async function fetchBiliBili(uid: string): Promise<BiliBiliStats> {
   const t = await Promise.all([
-    axios.get('https://api.bilibili.com/x/space/arc/search', { params: { mid: uid } }),
-    axios.get('https://api.bilibili.com/x/space/acc/info', { params: { mid: uid } }),
-    axios.get('https://api.bilibili.com/x/relation/stat', { params: { vmid: uid } }),
+    request.get('https://api.bilibili.com/x/space/arc/search', { params: { mid: uid } }),
+    request.get('https://api.bilibili.com/x/space/acc/info', { params: { mid: uid } }),
+    request.get('https://api.bilibili.com/x/relation/stat', { params: { vmid: uid } }),
   ]);
   const res = t.map(x => x.data.data);
+  console.log(res[1])
   return {
     username: res[1].name,
     followers: res[2].follower,
